c74364b0c9777eb670d0afc8fe7f422dd3122643
Wyatt <wyatt.allan@philips.com>
2022-01-06 22:29:48 -0700

    Saving new and saved templates to user object in firebase

--- src/app/shared/all-templates/templates.service.ts
+++ src/app/shared/all-templates/templates.service.ts
@@ -2,19 +2,24 @@ import { Injectable } from "@angular/core";
 import { AngularFirestore } from "@angular/fire/compat/firestore";
 import { AuthService } from "src/app/auth/auth.service";
 import { User } from "src/app/auth/user.model";
-import { map } from 'rxjs/operators';
-import { BehaviorSubject } from "rxjs";
 
 @Injectable({providedIn: 'root'})
 export class TemplatesService {
     public currentTemplateUUID: string;
-    constructor(private afStore: AngularFirestore, private authSvc: AuthService) {}
-
-    saveTemplateToFirebase(templateType: string) {
+    user: User;
+    constructor(private afStore: AngularFirestore, private authSvc: AuthService) {
         this.authSvc.currentUser.subscribe((user: User) => {
-            this.afStore.doc(`users/${user.uId}`).get().subscribe(document => {
-                console.log(document.get('data'));
-            })
+            this.user = user;
         })
+    }
+
+    saveTemplateToFirebase() {
+        this.afStore.doc(`users/${this.user.uId}`).update(this.user)
+            .then(() => {
+                console.log('SUCCESS')
+            })
+            .catch(err => {
+                console.error(err);
+            })
     }
 }
