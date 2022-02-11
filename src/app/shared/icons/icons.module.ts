08228f242fd7132b092efb68355c776d918afbd1
Wyatt <wyatt.allan@philips.com>
2022-02-11 14:48:10 -0700

    Added a plus icon. Color isn't changing to white

--- src/app/shared/icons/icons.module.ts
+++ src/app/shared/icons/icons.module.ts
@@ -2,13 +2,22 @@ import { NgModule } from "@angular/core";
 import { FacebookIconComponent } from "./facebook-icon/facebook-icon.component";
 import { GoogleIconComponent } from "./google-icon/google-icon.component";
 import { InstagramIconComponent } from "./instagram-icon /instagram-icon.component";
+import { PlusIconComponent } from "./plus-icon/plus-icon.component";
 
 @NgModule ({
-    declarations: [GoogleIconComponent, FacebookIconComponent, InstagramIconComponent],
-    exports: [GoogleIconComponent, FacebookIconComponent, InstagramIconComponent]
+    declarations: [
+        GoogleIconComponent, 
+        FacebookIconComponent, 
+        InstagramIconComponent,
+        PlusIconComponent
+    ],
+    exports: [
+        GoogleIconComponent, 
+        FacebookIconComponent, 
+        InstagramIconComponent,
+        PlusIconComponent
+    ]
 })
 
-export class IconsModule {
-
-};
+export class IconsModule {};
 
